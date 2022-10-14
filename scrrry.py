6d75b7dc00e11f586d004046a690d255fab44579
DGalbichek <DGalbichek@users.noreply.github.com>
2022-10-14 11:47:11 +0100

    update proxy table xpath

--- scrrry.py
+++ scrrry.py
@@ -520,7 +520,7 @@ class Scrape_Db():
 
         proxies=[]
 
-        for row in h.xpath('//table[@id="proxylisttable"]/tbody/tr'):
+        for row in h.xpath('//div[@class="table-responsive fpl-list"]/table/tbody/tr'):
             proxies.append({})
             
             try:
